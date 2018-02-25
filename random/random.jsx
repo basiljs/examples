b8561557e28da423dab7f2bb9383c47005b34051
trych <timorychert@23x5.net>
2018-02-25 15:05:40 +0100

    Updated random script for b-less

--- random/random.jsx
+++ random/random.jsx
@@ -1,18 +1,14 @@
-// @includepath "~/Documents/;%USERPROFILE%Documents";
-// @include "basiljs/basil.js";
+// @includepath ~/Documents/;%USERPROFILE%Documents;
+// @include basiljs/basil.js;
 
 function draw() {
-  var count = 23;
 
-  b.doc();
-  b.println(b.width + " x " + b.height);
+  clear(doc());
 
   for (var i = 0; i < 23; i++) {
-    var x = b.random(0, b.width);
-    var y = b.random(0, b.height);
-    var size = b.random(10, 123);
-    b.ellipse(x, y, size, size);
+    var x = random(0, width);
+    var y = random(0, height);
+    var size = random(10, 123);
+    ellipse(x, y, size, size);
   }
 }
-
-b.go();
