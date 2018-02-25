a02e3b2e691a5523f37c7361c366cc65caf7bb01
trych <timorychert@23x5.net>
2018-02-25 20:56:15 +0100

    Improvements to and renaming of randomFonts.jsx

--- typography/randomFonts.jsx
+++ typography/randomFonts.jsx
@@ -2,12 +2,18 @@
 // @include basiljs/basil.js;
 
 function draw() {
-  var textFramesCount = 20;
+  units(PT);
+
+  var count = 20;
+  var tfWidth = 300;
+  var tfHeight = 20;
+
+  // load all installed fonts into a variable
   var fonts = app.fonts;
 
-  for (var i = 0; i < textFramesCount; i++) {
-    var posX = random(0, 600);
-    var posY = random(0, 850);
+  for (var i = 0; i < count; i++) {
+    var posX = random(width - tfWidth);
+    var posY = random(height - tfHeight);
     var randomFontSize = round(random(2, 20));
     var randomIndex = floor(random(app.fonts.length));
     var fontName = fonts[randomIndex].fullName;
@@ -15,6 +21,6 @@ function draw() {
 
     textSize(randomFontSize);
     textFont(fonts[randomIndex]);
-    var textFrame = text(fontName + " " + randomFontSize, posX, posY, 300, 20);
+    var textFrame = text(fontName + " " + randomFontSize, posX, posY, tfWidth, tfHeight);
   }
 }
